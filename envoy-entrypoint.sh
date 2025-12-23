d85b1755ec50f2e5cb18cd99b9731a0e7fed8fe8
Prathamesh (Varun) Kulkarni <varunkulkarni@gmail.com>
2025-12-23 14:00:42 -0500

    Env file (#24)
    * initial env var
    
    * its working on port 10000
    
    * fix extension
    
    * decouple ports
    
    * simplify configurable ports

---
+++ envoy-entrypoint.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env sh
+set -eu
+
+# Fail fast if required vars are missing
+: "${MESSAGING_APP_HOST:?Must set MESSAGING_APP_HOST}"
+: "${MESSAGING_APP_PORT:?Must set MESSAGING_APP_PORT}"
+
+envsubst < /etc/envoy/envoy.template.yaml > /etc/envoy/envoy.yaml
+
+echo "=== Rendered /etc/envoy/envoy.yaml ==="
+sed -n '1,200p' /etc/envoy/envoy.yaml
+echo "=== Starting Envoy ==="
+
+exec envoy -c /etc/envoy/envoy.yaml --log-level info
