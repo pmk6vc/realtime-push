2d89de7b409efcc3a00ccb4b8a573054c80efff3
Varun Kulkarni <varunkulkarni@gmail.com>
2025-12-24 22:13:23 -0500

    spotless and pinning specific envoy version

--- src/integrationTest/java/testutils/TestEchoController.java
+++ src/integrationTest/java/testutils/TestEchoController.java
@@ -2,15 +2,14 @@ package testutils;
 
 import io.micronaut.context.annotation.Requires;
 import io.micronaut.http.annotation.*;
-
 import java.util.Map;
 
 @Requires(env = "test")
 @Controller("/__test")
 public class TestEchoController {
 
-    @Get("/whoami")
-    public Map<String, Object> whoami(@Header(name = "x-user-id", defaultValue = "") String xUserId) {
-        return Map.of("xUserId", xUserId);
-    }
+  @Get("/whoami")
+  public Map<String, Object> whoami(@Header(name = "x-user-id", defaultValue = "") String xUserId) {
+    return Map.of("xUserId", xUserId);
+  }
 }
